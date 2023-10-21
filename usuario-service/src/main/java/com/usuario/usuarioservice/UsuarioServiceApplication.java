4feb30b3d68aebab68aeda4229a3c43983a39258
YamilaAbad <yamiabad@hotmail.com>
2023-10-21 17:27:56 -0300

    Configuraciones general + configuración de servicio Eureka

---
+++ usuario-service/src/main/java/com/usuario/usuarioservice/UsuarioServiceApplication.java
@@ -0,0 +1,13 @@
+package com.usuario.usuarioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioServiceApplication.class, args);
+	}
+
+}
